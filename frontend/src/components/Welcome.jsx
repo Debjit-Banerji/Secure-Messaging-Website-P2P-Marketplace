d4b78f686cd01fef2e6611839eae83f4b718dc2b
Debjit Banerji <101322330+Debjit-Banerji@users.noreply.github.com>
2025-02-19 21:52:11 +0530

    Add files via upload

---
+++ frontend/src/components/Welcome.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+
+export default function Welcome() {
+  const [userName, setUserName] = useState("Eric");
+  // useEffect(async () => {
+  //   setUserName(
+  //     await JSON.parse(
+  //       localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+  //     ).username
+  //   );
+  // }, []);
+  return (
+    <Container>
+      <TextWrapper>
+        <h1>
+          Welcome, <span>{userName}!</span>
+        </h1>
+        <h3>Please select a chat to Start messaging.</h3>
+      </TextWrapper>
+      <img src={Robot} alt="" />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: row;
+  padding-left: 5rem;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #4e0eff;
+  }
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
