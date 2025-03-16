43452fdc8fc177d056205a48163499fb6203635c
Debjit Banerji <101322330+Debjit-Banerji@users.noreply.github.com>
2025-03-16 20:31:44 +0530

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -36,10 +36,14 @@ class ChatMessage(models.Model):
     receiver = models.ForeignKey(User, related_name="received_messages", on_delete=models.CASCADE)
     message = models.TextField()
     nonce = models.CharField(blank=True, null=True)
+    type = models.CharField(blank=True, null=True)
+    fileType = models.CharField(blank=True, null=True)
+    fileName = models.CharField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.sender} -> {self.receiver}: {self.message[:30]}"
+
 
 class OTP(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -61,9 +65,9 @@ class OTP(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='product_images/')
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     sold = models.BooleanField(default=False)
 
