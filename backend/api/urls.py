99d5aa1355688ab3eab780c636c5f650cd139f64
Debjit Banerji <101322330+Debjit-Banerji@users.noreply.github.com>
2025-03-25 17:30:27 +0530

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import MyTokenObtainPairView, register_user, search_users, update_profile, search_friends_groups, get_chats, send_chat, sample_api, add_contact, list_contacts, remove_contact, profile_view, send_otp_email, verify_otp, products, buy_product
 from . import views
+
 urlpatterns = [
     path('hello/', sample_api, name='hello'),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
@@ -12,6 +13,7 @@ urlpatterns = [
     path('search-friends-groups/', search_friends_groups, name='search-friends-groups'),
     path('chats/<int:user_id>/', get_chats, name='get-chats'),
     path('send-chat/', send_chat, name='send-chat'),
+
     path('contacts/add/', add_contact, name='add-contact'),
     path('contacts/', list_contacts, name='list-contacts'),
     path('contacts/remove/', remove_contact, name='remove-contact'),
@@ -25,5 +27,8 @@ urlpatterns = [
     path('groups/<int:group_id>/add_member/', views.add_group_member, name='add_group_member'),
     path('groups/<int:group_id>/remove_member/<int:member_id>/', views.remove_group_member, name='remove_group_member'),
     path('groups/<int:group_id>/', views.get_group_details, name='get_group_details'),
+    path('group-membership/<int:group_id>/', views.get_group_membership, name='get_group_membership'),
+    path('groups/<int:group_id>/messages/', views.get_group_messages, name='get_group_messages'),
     path('groups/', views.list_user_groups, name='list_user_groups'),
+    path('groups/<int:group_id>/send_message/', views.send_group_message, name='send_group_message'),
 ]
